28ba8d16348f0eb64c48b19117a6febf6f9cc166
dekelshoot <juniortchoupe5@gmail.com>
2023-04-23 02:15:33 +0100

    history

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -3,6 +3,7 @@ import { RouterService } from '../../services/router.service';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { DealerService } from '../../services/dealer.service';
+import { RoutesService } from 'src/app/services/routes.service';
 
 @Component({
   selector: 'app-header',
@@ -12,13 +13,15 @@ import { DealerService } from '../../services/dealer.service';
 export class HeaderComponent implements OnInit {
   dealer!: any;
   dealerSubscription!: Subscription;
-  db = "https://placecmr.onrender.com"
+  db !: string;
   constructor(public routerService: RouterService, public router: Router,
-    private dealerService: DealerService) { }
+    private dealerService: DealerService,
+    private routesService: RoutesService) { }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+    this.db = this.routesService.baseUrl
     console.log(this.router.url)
     if (localStorage.getItem("placeUser") != null) {
       const local: any = localStorage.getItem("placeUser")
